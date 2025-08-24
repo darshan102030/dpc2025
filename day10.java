faeecfbed50c15aa7d4818b13a283434060bfad6
Darshan Gosavi <gosavidarshan024@gmail.com>
2025-08-24 22:51:21 +0530

    Update day10.java

--- day10.java
+++ day10.java
@@ -1,25 +1,37 @@
 import java.util.*;
 
-class AnagramsGroup {
-    public List<List<String>> anagramsGroup(String[] strs) {
+class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            // sorting string
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            // creating unique key
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int num : count) {
+                keyBuilder.append("#").append(num); 
+            }
+            String key = keyBuilder.toString();
 
-            // adding words
+            // Adding words to map
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
         return new ArrayList<>(map.values());
     }
-  
+
     public static void main(String[] args) {
-        AnagramsGroup ga = new AnagramsGroup();
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
-        System.out.println(ga.anagramsGroup(strs));
+        GroupAnagrams ga = new GroupAnagrams();
+
+        String[] input1 = {"eat","tea","tan","ate","nat","bat"};
+        String[] input2 = {"abc","bca","cab","xyz","zyx","yxz"};
+        String[] input3 = {"a"};
+
+        System.out.println(ga.groupAnagrams(input1));
+        System.out.println(ga.groupAnagrams(input2));
+        System.out.println(ga.groupAnagrams(input3));
     }
 }
